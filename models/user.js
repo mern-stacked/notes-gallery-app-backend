12043dab9b43c74b406fa77238ce50e0e4115aef
eshwagadkar <eshwagadkar007@gmail.com>
2023-05-30 19:39:14 +0530

    user schema update

--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    image: { type: String, required: true },
     designation: { type:String, required: true },
     notes: [{ 
         type: mongoose.Types.ObjectId,
