47eda8006496e5310c04b418a42778744159d7c1
eshwagadkar <eshwagadkar007@gmail.com>
2023-05-31 17:11:32 +0530

    password hash and salt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const muvalidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -14,6 +15,49 @@ const userSchema = new Schema({
         ref: 'Note'
     }]
 });
+
+// Preprocessing of the password : Hashing and Salting the password before creating a user or instance of the user schema
+userSchema.pre('save', function(next) {
+
+   const user = this;
+
+   if(!user.isModified('password')){
+    return next();
+   } 
+
+   bcrypt.genSalt(10, (err, salt) => {
+
+     if(err) { return next(err); }
+
+     bcrypt.hash(user.password, salt, (err, hash) => {
+        
+        if(err) { return next(err); }
+
+        user.password = hash; 
+        mext();
+    });
+    
+ })
+
+});
+
+// Compare the passord generated during signup with the one while login 
+userSchema.methods.comparePassword = function(candidatePassword) {
+    
+    const user = this;
+
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+
+            if(err) return reject(err) 
+            
+            if(!isMatch) return reject(false)
+
+            resolve(true);
+
+        })
+    });
+}
 
 userSchema.plugin(muvalidator);
 
